36b0ec980de6eaf23a08f0bab178fb104b93529f
Darkliself <4e486bux@gmail.com>
2019-04-24 16:49:32 +0300

    init

---
+++ mainView.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ViewPattern.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setObjectName("pushButton")
+        self.gridLayout.addWidget(self.pushButton, 0, 0, 1, 1)
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.tab)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.tabWidget.addTab(self.tab_2, "")
+        self.gridLayout.addWidget(self.tabWidget, 1, 0, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(0)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "Tab 1"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "Tab 2"))
+
---
+++ webView.py
@@ -0,0 +1,83 @@
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEnginePage, QWebEngineSettings
+#from PyQt5.QtCore import QUrl
+import os
+
+
+# testing webView
+# self.textBrowser = QtWidgets.QTextBrowser(self.aceTab)
+# self.textBrowser.setObjectName("textBrowser")
+# self.gridLayout_2.addWidget(self.textBrowser, 0, 0, 1, 1)
+
+# self.webView = QWebView()
+# self.webView.setHtml(testText)
+# self.gridLayout_2.addWidget(self.webView, 0, 0, 1, 1)
+
+class editableView(QWebEngineView):
+    def __init__(self):
+        super().__init__()
+        page = self.page()
+
+
+# end of testing webView
+class AceWebView:
+    def __init__(self, layout):
+        testText = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+    <title>Ace Winforms</title>
+    <style type="text/css" media="screen">
+        #editor {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+        }
+    </style>
+</head>
+<body>
+    <div id="editor">
+        some text here
+    </div>
+    <div id="commandline" style="position: absolute; bottom: 10px; height: 20px; width: 800px;"></div>
+    <!--<script src="http://cdn.jsdelivr.net/ace/1.1.01/min/ace.js" type="text/javascript" charset="utf-8"></script>-->
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.3.3/ace.js" type="text/javascript" charset="utf-8"></script>
+
+    <script>
+       var editor = ace.edit("editor");
+    editor.setTheme("ace/theme/ambiance");
+    editor.getSession().setMode("ace/mode/csharp");
+    </script>
+</body>
+</html>
+                   """
+        self.browserView = QWebEngineView()
+        layout.addWidget(self.browserView, 0, 0, 1, 1)
+        ##file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "ace_page.html"))
+        ##self.someTest.load(QUrl.fromLocalFile(file_path))
+        self.browserView.setHtml(testText)
+        somez = self.browserView.page().runJavaScript("""
+                    //alert('its wotking')
+                """)
+
+        def write_html_to_file(html):
+            test = html
+            print(test)
+
+        self.browserView.page().toHtml(write_html_to_file)
+
+        # print(testText)
+
+
+
+        #somez2 = self.someTest.page().toHtml()
+
+
+
+        # self.someTest.QWebEnginePage.triggerAction(9)
+
+# test.triggerPageAction(9)
+# <script src="http://d1n0x3qji82z53.cloudfront.net/src-min-noconflict/ace.js" type="text/javascript" charset="utf-8"></script>
